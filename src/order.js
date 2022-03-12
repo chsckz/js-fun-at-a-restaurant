4cd3a702297f2d121cee1e88a78e69cfa38718b5
James Lee <lee.jamests@gmail.com>
2022-03-12 18:37:40 -0500

    Add refundOrder Function
    Passed 23 tests

--- src/order.js
+++ src/order.js
@@ -6,15 +6,23 @@ function takeOrder(order, deliveryOrders) {
   };
 }
 
-
-
+function refundOrder(order, deliveryOrders) {
+  let deleteIndex;
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    let temp = deliveryOrders[i].orderNumber;
+        if (temp === order) {
+      deleteIndex = i
+    }
+  }
+  deliveryOrders.splice(deleteIndex,1)
+} 
 
 
 
 
 module.exports = {
   takeOrder,
-  // refundOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
