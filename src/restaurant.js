4be10d3c7ab5b598f5c69e051a4ec5e7883a391a
James Lee <lee.jamests@gmail.com>
2022-03-14 13:36:00 -0400

    Add removeMenuItem Function
    All restaurant.js tests passed!

--- src/restaurant.js
+++ src/restaurant.js
@@ -25,9 +25,40 @@ function addMenuItem(restaurantName, addDish) {
   }
 }
 
+function removeMenuItem(restaurantName, removeDish, mealType) {
+  let breakfast = restaurantName.menus.breakfast;
+  let lunch = restaurantName.menus.lunch;
+  let dinner = restaurantName.menus.dinner;
+  if (mealType.toLowerCase() == 'breakfast') {
+    for (i = 0; i < breakfast.length; i++) {
+      if (removeDish == breakfast[i].name) {
+        breakfast.splice(i, 1);
+        return `No one is eating our ${removeDish} - it has been removed from the ${mealType} menu!`;
+      }
+    }
+  };
+  if (mealType.toLowerCase() == 'lunch') {
+    for (i = 0; i < lunch.length; i++) {
+      if (removeDish == lunch[i].name) {
+        lunch.splice(i, 1);
+        return `No one is eating our ${removeDish} - it has been removed from the ${mealType} menu!`;
+      }
+    }
+  };
+  if (mealType.toLowerCase() == 'dinner') {
+    for (i = 0; i < dinner.length; i++) {
+      if (removeDish == dinner[i].name) {
+        dinner.splice(i, 1);
+        return `No one is eating our ${removeDish} - it has been removed from the ${mealType} menu!`;
+      }
+    }
+  } else {
+    return `Sorry, we don't sell ${removeDish}, try adding a new recipe!`
+  };
+}
 
 module.exports = {
   createRestaurant, 
   addMenuItem,
-  // removeMenuItem
+  removeMenuItem
 }
