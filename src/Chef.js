649209e4314debfbc236ac8a0ac91882baeed918
James Lee <lee.jamests@gmail.com>
2022-03-14 16:01:19 -0400

    Add Chef Class & Functions
    All tests passed!

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,23 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+  this.name = name;    
+  this.restaurant = restaurant;
+  };
+  greetCustomer(name, morning) {
+    if (morning == true) {
+      return `Good morning, ${name}!`
+    }
+      return `Hello, ${name}!`
+  };
+  checkForFood(foodItem) {
+    let b = this.restaurant.menus.breakfast;
+    let l = this.restaurant.menus.lunch;
+    let d = this.restaurant.menus.dinner;
+    if (b.includes(foodItem) || l.includes(foodItem) || d.includes(foodItem)) {
+      return `Yes, we're serving ${foodItem.name} today!`;
+    } else {
+      return `Sorry, we aren't serving ${foodItem.name} today.`
+    }
   }
 }
 
