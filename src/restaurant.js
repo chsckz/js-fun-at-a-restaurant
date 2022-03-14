544b083418143351e4a3664e9dcf5771d7a90f3d
James Lee <lee.jamests@gmail.com>
2022-03-13 23:20:30 -0400

    Add addMenuItem Function - WIP
    addMenuItem does not currently add objects correctly to the matching menu type.

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,29 @@
+function createRestaurant(restaurantName) {
+  const newRestaurant = {
+    name: restaurantName,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  };
+  return newRestaurant;
+}
+
+function addMenuItem(restaurantName, addDish) {
+  if (addDish.type == 'breakfast') {
+    restaurantName.menus.breakfast.push(addDish);
+  }
+  if (addDish.type == 'lunch') {
+    restaurantName.menus.lunch.push(addDish);
+  }
+  if (addDish.type == 'dinner') {
+    restaurantName.menus.dinner.push(addDish);
+}
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant, 
+  addMenuItem,
   // removeMenuItem
 }
